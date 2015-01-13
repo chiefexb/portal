ae81a842923ec85a7edcf619b192316fbbae9227
chiefexb <chief_exb@mail.ru>
2015-01-13 23:52:54 +0300

    Модель Поручения и справочники очередной апдейт

--- portal/control/models.py
+++ portal/control/models.py
@@ -5,6 +5,9 @@ class Perodic_Type (models.Model):
     Name=CharField(max_length=50,verbose_name=u'Название')
 class Order_Kind (models.Model):
     Name=CharField(max_length=100,verbose_name=u'Название')
+class Departament (models.Model):
+    Dept_Code=CharField(max_length=5,verbose_name=u'Код подразделения')
+    Dept_Name=CharField(max_length=400,verbose_name=u'Название подразделения')
 class Assignment (models.Model):
     Is_Active = models.BooleanField(verbose_name=u'Активно (периодическое)')
     Is_Archive = models.BooleanField(verbose_name=u'Архивное')
@@ -13,8 +16,10 @@ class Assignment (models.Model):
     Create_Date=models.DateTimeField(verbose_name=u'Дата создания')
     Dep_Code=CharField(max_length=400,verbose_name=u'Код подразделения')
     Is_Periodic=CharField(max_length=400,verbose_name=u'Периодическое')
-    Perodic_Type=models.ForeignKey( Perodic_Type,verbose_name=u'Тип периода')
-    Periodic_Size=IntegerField(verbose_name=u'Периодичноcть')
+    Perodic_Type=models.ForeignKey('Perodic_Type',Blank=True,Null=True,verbose_name=u'Тип периода')
+    Period_End_Date=models.DateTimeField(Blank=True,Null=True,verbose_name=u'Дата окончания периодичности')
+    Periodic_Size=IntegerField(Blank=True,Null=True,verbose_name=u'Периодичноcть')
+    Executor=ManyToMany (Departament,verbose_name=u'Исполнител(и)')
     Suser_Fio=CharField(max_length=400,verbose_name=u'Кто создал')
     Order_Kind=models.ForeignKey(Order_Kind,verbose_name=u'Тип документа')
 
